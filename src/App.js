1dd2ab7f107f6af7c64cb64bdd1a216bd1860954
denisdelaki <denisdelaki@gmail.com>
2022-12-04 14:17:39 +0300

    added services

--- src/App.js
+++ src/App.js
@@ -3,21 +3,59 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='portfolio'>
+        <h1>Portfolio</h1>
+        <h3>Kipyegon Denis </h3>
+        <img src='' alt='profile pic' />
+       <h1> Services </h1>
+        <div>
+          <div>
+          <h3>Front-end web development </h3>
+          <h4>Languages</h4>
+          <ol>
+            <li>Javascript
+              <ol>
+                <li>ReactJs</li>
+                <li>Angular</li>
+              </ol>
+            </li>
+            <li>HTML</li>
+            <li> CSS
+              <ol>
+                <li>Boostrap</li>
+                <li>TailwindCSS</li>
+                <li>SASS</li>
+              </ol>
+            </li>
+          </ol>
+          </div>
+          <div>
+            <h3>Backend Web Development </h3>
+            <h4>Languages</h4>
+            <ol>
+              <li>Ruby on Rails </li>
+              <li>NodeJs</li>
+
+            </ol>
+          </div>
+          <div>
+            <h2>Web UX/UI Design </h2>
+            <h3>Tools</h3>
+
+            <ol>
+              <li>Figma</li>
+              <li>Sketch</li>
+            </ol>
+          </div>
+          <div>
+            <h2>Technical Writing</h2>
+          </div>
+    <div>
+        <p><i>facebook</i></p>
+        <p><i>LinkedIn</i></p>
+        <p><i>email</i></p>
+    </div>
+    </div>
     </div>
   );
 }
